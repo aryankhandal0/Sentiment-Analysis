30c660b7d236c0b3d191d1a5d83c40d4dea13357
Aryan Khandal <aryankhandal0@gmail.com>
2019-05-20 16:47:12 +0530

    2 branches completed 1 more to go

--- convertwordtovec.py
+++ convertwordtovec.py
@@ -11,7 +11,6 @@ import numpy as np
 from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
 from keras.preprocessing.sequence import pad_sequences
-#import and head, info
 dfy = pd.read_csv('sentiment labelled sentences/yelp_labelled.txt',names=['sentence', 'label'], sep='\t')
 dfa = pd.read_csv('sentiment labelled sentences/amazon_cells_labelled.txt',names=['sentence', 'label'], sep='\t')
 dfi = pd.read_csv('sentiment labelled sentences/imdb_labelled.txt',names=['sentence', 'label'], sep='\t')
@@ -19,17 +18,12 @@ df = pd.concat([dfy,dfa,dfi])
 from sklearn.model_selection import train_test_split
 xtrain,xtest,ytrain,ytest = train_test_split(df['sentence'],df['label'])
 w2v = Word2Vec.load('word2vecmodel.bin')
-#def preprocess_x(data):
-#    cdf = pd.DataFrame(data)
-#    cdf.reset_index(inplace=True)
-#    cdf.drop('index',axis=1,inplace=True)
-#    return cdf
 def convtraintest2vec(data):
     i = 0
     ind = 0
     sent_list_vec = []
     list_ind = []
-    for x in xtrain:
+    for x in data:
         ind = ind + 1
         sent_list = []
         words = word_tokenize(x)
@@ -48,33 +42,37 @@ def convtraintest2vec(data):
         else:
             list_ind.append(ind)
     return sent_list_vec, list_ind
-#def preprocess_y(data,list_ind):
-#    cdf = pd.DataFrame(data)
-#    cdf.reset_index(inplace=True)
-#    cdf.drop('index',axis=1,inplace=True)
-#    cdf.drop(list_ind,inplace=True)
-#    return list(cdf)
-#xtrain = preprocess_x(xtrain)
-#xtest = preprocess_x(xtest)
 sent_train, train_missing = convtraintest2vec(xtrain)
 sent_test, test_missing = convtraintest2vec(xtest)
-#trainY = preprocess_y(ytrain,train_missing)
-#testY = preprocess_y(ytest,test_missing)
 maxi = []
 for l in sent_train:
     maxi.append(len(l))
 max_l = max(maxi)
+maxi = []
+for l in sent_test:
+    maxi.append(len(l))
+max_t = max(maxi)
 
 trainX = pad_sequences(sent_train,maxlen = max_l)
-testX = pad_sequences(sent_test,maxlen = max_l)
+testX = pad_sequences(sent_test,maxlen = max_t)
 print("Xtrain_Original: " + str(len(xtrain)))
 print("Xtest_Original: " + str(len(xtest)))
 print("Xtrain_Preprocessed: " + str(len(trainX)))
 print("Xtest_Preprocessed: " + str(len(testX)))
 print("Index missing in train: " + str(list(train_missing)))
 print("Index missing in test: " + str(list(test_missing)))
-
-
+trainY = []
+testY = []
+for index, item in enumerate(ytrain):
+    if index not in train_missing:
+        trainY.append(item)
+for index, item in enumerate(ytest):
+    if index not in test_missing:
+        testY.append(item)
+print("trainY_Original: " + str(len(ytrain)))
+print("testY_Original: " + str(len(ytest)))
+print("trainY_Preprocessed: " + str(len(trainY)))
+print("testY_Preprocessed: " + str(len(testY)))
     
 
         
